2ec157a17c0885c9b675510ae0c9d041940bf1cd
Sinovuyo <cnovuyo10@gmail.com>
2022-03-30 16:42:50 +0200

    added the filter

--- cars.js
+++ cars.js
@@ -1,4 +1,6 @@
 const carsElem = document.querySelector(".cars");
+const colorsElem = document.querySelector("#color");
+const makesElem = document.querySelector("#make");
 
 axios
 .get("https://api-tutor.herokuapp.com/v1/cars")
@@ -6,7 +8,7 @@ axios
     result.data.forEach(car => {
                 const li = document.createElement("tr");
                 li.innerHTML = `<tr>
-                    <td> ${car.model}</td>
+                     <td>${car.model}
                     <td> ${car.color}</td>
                     <td> ${car.make}</td>
                 </tr>`
@@ -14,3 +16,36 @@ axios
                 carsElem.appendChild(li);
             });
 })
+
+
+axios
+.get("https://api-tutor.herokuapp.com/v1/colors")
+.then(function (result) {
+    result.data.forEach(color => {
+                const li = document.createElement("option");
+                li.innerHTML = `
+                ${color}`
+                colorsElem.appendChild(li);
+            });
+         
+})
+
+
+axios
+.get("https://api-tutor.herokuapp.com/v1/makes")
+.then(function (result) {
+    result.data.forEach(make => {
+                const li = document.createElement("option");
+                li.innerHTML = `
+                ${make}`
+                
+                makesElem.appendChild(li);
+                // const subBtn = document.createElement("submit");
+               
+            });
+         
+})
+
+
+
+
