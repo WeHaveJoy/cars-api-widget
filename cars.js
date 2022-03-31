317cbc92edef60e06ba2a632e2231e0cfedf69e3
Sinovuyo <cnovuyo10@gmail.com>
2022-03-31 16:26:44 +0200

    added functionality on the filter button

--- cars.js
+++ cars.js
@@ -1,50 +1,87 @@
 const carsElem = document.querySelector(".cars");
 const colorsElem = document.querySelector("#color");
 const makesElem = document.querySelector("#make");
+const filterElem = document.querySelector(".btnFilter");
 
 axios
-.get("https://api-tutor.herokuapp.com/v1/cars")
-.then(function (result) {
+  .get("https://api-tutor.herokuapp.com/v1/cars")
+  .then(function (result) {
     result.data.forEach(car => {
-                const li = document.createElement("tr");
-                li.innerHTML = `<tr>
-                     <td>${car.model}
+      const li = document.createElement("tr");
+      li.innerHTML = `<tr>
+                     <td>${car.model}</td>
                     <td> ${car.color}</td>
                     <td> ${car.make}</td>
+                    <td> ${car.reg_number}</td>
+                    <td> ${car.price}</td>
                 </tr>`
-                
-                carsElem.appendChild(li);
-            });
-})
+      carsElem.appendChild(li);
+    });
+  })
 
 
 axios
-.get("https://api-tutor.herokuapp.com/v1/colors")
-.then(function (result) {
+  .get("https://api-tutor.herokuapp.com/v1/colors")
+  .then(function (result) {
     result.data.forEach(color => {
-                const li = document.createElement("option");
-                li.innerHTML = `
+      const li = document.createElement("option");
+      li.innerHTML = `
                 ${color}`
-                colorsElem.appendChild(li);
-            });
-         
-})
+      colorsElem.appendChild(li);
+    });
 
+  })
 
-axios
-.get("https://api-tutor.herokuapp.com/v1/makes")
-.then(function (result) {
+  
+  axios
+  .get("https://api-tutor.herokuapp.com/v1/makes")
+  .then(function (result) {
     result.data.forEach(make => {
-                const li = document.createElement("option");
-                li.innerHTML = `
+      const li = document.createElement("option");
+      li.innerHTML = `
                 ${make}`
-                
-                makesElem.appendChild(li);
-                // const subBtn = document.createElement("submit");
-               
-            });
-         
+      makesElem.appendChild(li);
+    });
+
+  })
+
+
+
+filterElem.addEventListener("click", function () {
+  let showElem = document.querySelector('.show')
+  axios
+    .get("https://api-tutor.herokuapp.com/v1/cars")
+    .then(function (result) {
+      result.data.forEach(car => {
+        if (makesElem.value == car.make &&  colorsElem.value == car.color) {
+          const li = document.createElement("li");
+          
+          li.innerHTML = `
+        ${car.model}
+          `
+          showElem.appendChild(li)
+        }
+        else if (makesElem.value == car.make) {
+          const li = document.createElement("li");
+          
+          li.innerHTML = `
+        ${car.model}
+          `
+          showElem.appendChild(li)
+        }else if (colorsElem.value == car.color) {
+          const li = document.createElement("li");
+          
+          li.innerHTML = `
+        ${car.model}
+          `
+          showElem.appendChild(li)
+        }
+      });
+
+    })
 })
+
+
 
 
 
